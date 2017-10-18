9f682cb976c481f03f1db49b477ceff0d6cf304b
tim <timothee.napoli@laposte.net>
2017-10-19 01:25:43 +0200

    pyticket.commands: add the configure command.

--- pyticket.py
+++ pyticket.py
@@ -5,12 +5,18 @@ PyTicket main program.
 import sys
 from pyticket.command import command, argument, print_usage, execute_argv
 from pyticket.commands.init import init
+from pyticket.commands.configure import configure
 
 COMMANDS = [
     command(
         "init",
         "Initialize a pyticket repository",
         init
+    ),
+    command(
+        "configure",
+        "Configure the pyticket repository",
+        configure
     )
 ]
 
@@ -21,11 +27,7 @@ def main():
         print_usage(sys.argv[0], COMMANDS)
         sys.exit(0)
 
-    try:
-        execute_argv(COMMANDS, args)
-    except Exception as ex:
-        print("error: {}".format(str(ex)))
-        sys.exit(1)
+    execute_argv(COMMANDS, args)
 
 if __name__ == '__main__':
     main()
---
+++ pyticket/commands/configure.py
@@ -0,0 +1,8 @@
+from pyticket.utils import configuration, get_home_path
+
+def configure(argv,
+              what : "Which configuration key to configure",
+              value : "Key value"):
+    config = configuration.load(get_home_path())
+    config.set_value(what, value)
+    config.save(get_home_path())
