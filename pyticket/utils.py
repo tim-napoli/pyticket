3722fc0b433efc5891ba44416c58c8a5e5340cfa
tim <timothee.napoli@laposte.net>
2017-10-19 01:24:05 +0200

    pyticket.utils: add the configuration class.

--- pyticket/utils.py
+++ pyticket/utils.py
@@ -7,3 +7,31 @@ def get_root_path(directory = "."):
 def get_home_path():
     return os.path.expanduser("~/.pyticket")
 
+class configuration:
+    ALLOWED_VALUES = ["editor"]
+
+    def __init__(self, values):
+        self.values = values
+
+    def set_value(self, name, value):
+        if name not in configuration.ALLOWED_VALUES:
+            raise ValueError(
+                "'{}' is not a valid configuration key".format(name)
+            )
+        self.values[name] = value
+
+    def to_json(self):
+        return self.values
+
+    def save(self, directory):
+        path = "{}/config.json".format(directory)
+        with open(path, "w+") as f:
+            f.write(json.dumps(self.to_json(), indent=4))
+
+    @staticmethod
+    def load(directory):
+        path = "{}/config.json".format(directory)
+        with open(path, "r") as f:
+            content = json.loads(f.read())
+            return configuration(content)
+
