454a07c509473e429123231e643d86872ee7f68a
tim <timothee.napoli@laposte.net>
2017-10-19 01:23:36 +0200

    pyticket.utils: add get_home_path function.

--- pyticket/utils.py
+++ pyticket/utils.py
@@ -1,2 +1,9 @@
+import json
+import os.path
+
 def get_root_path(directory = "."):
     return "{}/.pyticket".format(directory)
+
+def get_home_path():
+    return os.path.expanduser("~/.pyticket")
+
