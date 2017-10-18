5dfd70a7b6ee3a41dd224d472634e1c99caeb74b
tim <timothee.napoli@laposte.net>
2017-10-19 01:25:05 +0200

    pyticket.commands.init: create a default configuration file if needed.

--- pyticket/commands/init.py
+++ pyticket/commands/init.py
@@ -1,7 +1,8 @@
 import os
 import os.path
+import getpass
 
-from pyticket.utils import get_root_path
+from pyticket.utils import get_root_path, get_home_path, configuration
 
 def init(argv, directory : "The pyticket repository directory"):
     if os.path.isdir(get_root_path(directory)):
@@ -14,5 +15,13 @@ def init(argv, directory : "The pyticket repository directory"):
     os.mkdir("{}/".format(get_root_path(directory)))
     os.mkdir("{}/opened/".format(get_root_path(directory)))
     os.mkdir("{}/closed/".format(get_root_path(directory)))
+
+    # Create default configuration file if needed.
+    if not os.path.isdir(get_home_path()):
+        os.mkdir(get_home_path())
+        config = configuration({
+            "editor": "nano"
+        })
+        config.save(get_home_path())
 
     print("Pyticket repository created")
