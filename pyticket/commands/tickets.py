ea05ef1f99e53c147f4d10a7433a8c18085ecd58
tim <timothee.napoli@laposte.net>
2017-10-19 21:34:18 +0200

    pytocket.commands.tickets: allows to create a ticket using a template.

--- pyticket/commands/tickets.py
+++ pyticket/commands/tickets.py
@@ -2,12 +2,18 @@ import subprocess
 import os.path
 
 from pyticket.utils import (
-    configuration, get_home_path, get_opened_tickets_path
+    configuration, get_home_path, get_opened_tickets_path, expand_template
 )
 
-def create_ticket(argv, ticket_name : "The ticket name"):
-    config = configuration.load(get_home_path())
+def create_ticket(argv,
+                  ticket_name : "The ticket name",
+                  template : "The template to use" = None):
     ticket_path = "{}/{}".format(get_opened_tickets_path(), ticket_name)
+    if template:
+        content = expand_template(template, {"ticket": ticket_name})
+        with open(ticket_path, "w+") as f:
+            f.write(content)
+    config = configuration.load(get_home_path())
     subprocess.call([config.values["editor"], ticket_path])
 
 def edit_ticket(argv, ticket_name : "The ticket name"):
