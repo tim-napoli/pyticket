2e87207180bc421efb1d4bd1a7f446ee7d471d34
tim <timothee.napoli@laposte.net>
2017-10-22 13:46:18 +0200

    pyticket.utils: add documentation.

--- pyticket/utils.py
+++ pyticket/utils.py
@@ -6,9 +6,11 @@ from string import Template
 from pyticket import PyticketException
 
 def get_root_path(directory = "."):
+    """Returns the pyticket path of the given directory."""
     return "{}/.pyticket".format(directory)
 
 def get_home_path():
+    """Returns the user's home pyticket directory."""
     return os.path.expanduser("~/.pyticket")
 
 def get_opened_tickets_path(directory = "."):
@@ -21,6 +23,7 @@ def get_templates_path(directory = "."):
     return "{}/templates".format(get_root_path(directory))
 
 class configuration:
+    """The pyticket's configuration class."""
     ALLOWED_VALUES = ["editor"]
 
     def __init__(self, values):
@@ -49,6 +52,7 @@ class configuration:
             return configuration(content)
 
 def expand_template(template_name, values):
+    """Expand a template file using the given values dictionary."""
     path = "{}/{}".format(get_templates_path(), template_name)
     if not os.path.isfile(path):
         raise PyticketException(
@@ -59,6 +63,7 @@ def expand_template(template_name, values):
         return content.safe_substitute(**values)
 
 def read_ticket(directory, ticket_name):
+    """Returns the ticket content."""
     path = "{}/{}/{}".format(get_root_path(), directory, ticket_name)
     with open(path, "r") as f:
         return f.read()
@@ -70,11 +75,13 @@ def read_closed_ticket(ticket_name):
     return read_ticket("closed", ticket_name)
 
 def list_tickets(directory):
+    """List every tickets of the given directory."""
     path = "{}/{}".format(get_root_path(), directory)
     tickets = os.listdir(path)
     return tickets
 
 def get_ticket_tags(ticket_content):
+    """Returns tickets tags."""
     lines = ticket_content.splitlines()
     if not lines:
         return []
@@ -84,6 +91,7 @@ def get_ticket_tags(ticket_content):
     return []
 
 def find_ticket_directory(name):
+    """Find what is the ticket's directory."""
     opened_tickets = list_tickets("opened")
     if name in opened_tickets:
         return "opened"
@@ -93,12 +101,14 @@ def find_ticket_directory(name):
     raise PyticketException("there is no ticket '{}'".format(name))
 
 def get_ticket_parent(name):
+    """Returns the ticket's parent name."""
     path = name.split(".")
     if len(path) > 1:
         return ".".join(path[0:-1])
     return None
 
 def is_ticket(name):
+    """Checks the given ticket exist."""
     opened_tickets = list_tickets("opened")
     closed_tickets = list_tickets("closed")
     if name in opened_tickets or name in closed_tickets:
@@ -106,6 +116,7 @@ def is_ticket(name):
     return False
 
 def is_child_of(child, parent):
+    """Checks 'child' is a child of 'parent'."""
     if child.startswith(parent + "."):
         split_child = child.split(".")
         split_parent = parent.split(".")
@@ -113,6 +124,7 @@ def is_child_of(child, parent):
     return False
 
 def find_tickets_childs(directory, name):
+    """Find every tickets direct childs."""
     childs = []
     tickets = list_tickets(directory)
     for ticket in tickets:
@@ -121,6 +133,7 @@ def find_tickets_childs(directory, name):
     return childs
 
 def find_tickets_childs_deep(directory, name):
+    """Find every tickets childs and 'sub-childs'."""
     childs = find_tickets_childs(directory, name)
     deep = []
     for child in childs:
@@ -128,9 +141,11 @@ def find_tickets_childs_deep(directory, name):
     return childs + deep
 
 def is_closed_ticket(name):
+    """Check the given ticket is closed."""
     return name in list_tickets("closed")
 
 def write_ticket(directory, name, new_content):
+    """Write the given ticket content."""
     path = "{}/{}/{}".format(get_root_path(), directory, name)
     with open(path, "w+") as f:
         f.write(new_content)
