5034b2376a97be114102e04ebf38da30b6890a63
tim <timothee.napoli@laposte.net>
2017-10-26 22:40:48 +0200

    tests: aggregate every tests in a test suit.

---
+++ tests/__main__.py
@@ -0,0 +1,23 @@
+import unittest
+import sys
+import os
+
+from tests import (
+    test_configuration, test_generators, test_migrations, test_repository,
+    test_ticket
+)
+
+def suite():
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    suite.addTests(loader.loadTestsFromModule(test_configuration))
+    suite.addTests(loader.loadTestsFromModule(test_generators))
+    suite.addTests(loader.loadTestsFromModule(test_migrations))
+    suite.addTests(loader.loadTestsFromModule(test_repository))
+    suite.addTests(loader.loadTestsFromModule(test_ticket))
+    return suite
+
+if __name__ == "__main__":
+    sys.stdout = open(os.devnull, 'w')
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite())
