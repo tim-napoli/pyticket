30798940ea187bffe7ddb2786501eb07e241f15d
Timothée Napoli <timothee.napoli@laposte.net>
2025-12-05 11:57:46 +0100

    fixes encoding issue when reading tickets content

--- pyticket/repository.py
+++ pyticket/repository.py
@@ -311,7 +311,7 @@ class Repository:
                 "ticket '{}' has no content".format(name)
             )
         path = self.get_ticket_content_path(name)
-        with open(path, "r") as f:
+        with open(path, "r", encoding="utf-8") as f:
             return f.read()
 
     def switch_ticket_status(self, name, status):
